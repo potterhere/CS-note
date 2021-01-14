4b403edf7b98a74b275a286dc453dffd77b0a65f
Potter <472434557@qq.com>
2021-01-14 21:24:40 +0800

    [ADD] Singleton mode

---
+++ DesignPattern/CreateMode_Singleton/CreateMode_Singleton.cpp
@@ -0,0 +1,43 @@
+﻿// CreateMode_Singleton.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <mutex>
+using namespace std;
+
+class Singleton {
+public:
+    static Singleton* GetInstance()
+    {
+        static Singleton instance;
+        return &instance;
+    }
+
+public:
+    Singleton(const Singleton&) = delete;
+
+private:
+    Singleton() {};
+private:
+    static Singleton* instance;
+};
+
+Singleton* Singleton::instance = nullptr;
+
+int main()
+{
+    {
+        Singleton* singleton = Singleton::GetInstance();
+        cout << "0x" << hex << singleton << endl;
+    }
+
+    {
+        Singleton* singleton = Singleton::GetInstance();
+        cout << "0x" << hex << singleton << endl;
+    }
+
+    {
+        Singleton* singleton = Singleton::GetInstance();
+        cout << "0x" << hex << singleton << endl;
+    }
+}
