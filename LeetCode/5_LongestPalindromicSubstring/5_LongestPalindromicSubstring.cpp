51cd8b443396cabe372a5fe1ca655ff8ab01c59e
Potter <472434557@qq.com>
2021-02-03 22:21:50 +0800

    [ADD] LeetCode 5 ok

---
+++ LeetCode/5_LongestPalindromicSubstring/5_LongestPalindromicSubstring.cpp
@@ -0,0 +1,104 @@
+﻿// 5_LongestPalindromicSubstring.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include "../global.h"
+
+class Solution {
+public:
+     string longestPalindrome(string s) {
+        if (s.length() < 1) 
+            return "";
+
+        if (s.length() == 1)
+            return s;
+
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substr(start, end-start+1);
+    }
+
+    int expandAroundCenter(string s, int left, int right) {
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s[L] == s[R]) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
+    }
+};
+
+void test()
+{
+    {
+        Solution solu;
+        string strIn = "babad";
+        string strResult = solu.longestPalindrome(strIn);
+        
+        string strExpexted1 = "bab";
+        string strExpexted2 = "aba";
+        assert(strExpexted1 == strResult || strExpexted2 == strResult);
+    }
+    {
+        Solution solu;
+        string strIn = "cbbd";
+        string strResult = solu.longestPalindrome(strIn);
+
+        string strExpexted1 = "bb";
+        assert(strExpexted1 == strResult);
+    }
+    {
+        Solution solu;
+        string strIn = "a";
+        string strResult = solu.longestPalindrome(strIn);
+
+        string strExpexted1 = "a";
+        assert(strExpexted1 == strResult);
+    }
+    {
+        Solution solu;
+        string strIn = "ac";
+        string strResult = solu.longestPalindrome(strIn);
+
+        string strExpexted1 = "a";
+        assert(strExpexted1 == strResult || strResult == "c");
+    }
+    {
+        Solution solu;
+        string strIn = "bb";
+        string strResult = solu.longestPalindrome(strIn);
+
+        string strExpexted1 = "bb";
+        assert(strExpexted1 == strResult);
+    }
+    {
+        Solution solu;
+        string strIn = "apqjpwedlhmvvpexxnntxheeynxmgzwxhnhfdvziuxnuusymklgcacndoyhqkoahnkyaikohwkmnuphipftmzmihvmoetskioeypwjujvvusaxynzxxdugnebsisrtgeujkqkgwjuplijhluumqcdurovyjsbowmnqndejwkihzbbdyxjunkduyqeihektaknbmkzgnnmgywylulxwyywrvieqfenjeljofkqqqisdjsbfkvqgahxwkfkcucvrbbpyhwkfztjdboavtfynrudneieelwlcezqsuhmllcsadcnoyemsfdlrijoyj";
+        string strResult = solu.longestPalindrome(strIn);
+
+        string strExpexted1 = "wyyw";
+        assert(strExpexted1 == strResult);
+    }
+    {
+        Solution solu;
+        string strIn = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabcaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+        string strResult = solu.longestPalindrome(strIn);
+
+        string strExpexted1 = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+        assert(strExpexted1 == strResult);
+    }
+}
+
+int main()
+{
+    test();
+    std::cout << "Hello World!\n";
+}
