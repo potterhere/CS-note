d7829b0a9b5d6d0d73c1f4081f8178c7f7e6a931
Potter <472434557@qq.com>
2021-02-23 21:45:53 +0800

    [ADD] Leetcode 17 & 41

---
+++ LeetCode/17_LetterCombinationsOfAPhoneNumber/17_LetterCombinationsOfAPhoneNumber.cpp
@@ -0,0 +1,97 @@
+﻿// 17_LetterCombinationsOfAPhoneNumber.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include "../global.h"
+
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        // DFS
+        if (digits.size() == 0)
+            return {};
+
+        vector<string> vecResult;
+
+        ConsumeOne(digits, 0, "", vecResult);
+
+        return vecResult;
+    }
+
+    void ConsumeOne(string& digits, int index, string strCompose, vector<string>& vecResult)
+    {
+        if (index >= digits.size())
+        {
+            vecResult.push_back(strCompose);
+            strCompose = "";
+            return;
+        }
+
+        string sCur = mapping(digits[index]);
+        for (auto s : sCur)
+        {
+            ConsumeOne(digits, index + 1, strCompose + s, vecResult);
+        }
+    }
+
+private:
+    string mapping(char ch)
+    {
+        switch (ch)
+        {
+        case '1':
+            return "";
+        case '2':
+            return "abc";
+        case '3':
+            return "def";
+        case '4':
+            return "ghi";
+        case '5':
+            return "jkl";
+        case '6':
+            return "mno";
+        case '7':
+            return "pqrs";
+        case '8':
+            return "tuv";
+        case '9':
+            return "wxyz";
+        default:
+            return "";
+            break;
+        }
+    }
+};
+
+
+void test()
+{
+    {
+        Solution solu;
+        string digits = "23";
+        vector<string> vecResult = solu.letterCombinations(digits);
+        vector<string> vecExpected = {"ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"};
+        assert(vecExpected == vecResult);
+    }
+    {
+        Solution solu;
+        string digits = "";
+        vector<string> vecResult = solu.letterCombinations(digits);
+        vector<string> vecExpected = {  };
+        assert(vecExpected == vecResult);
+    }
+    {
+        Solution solu;
+        string digits = "2";
+        vector<string> vecResult = solu.letterCombinations(digits);
+        vector<string> vecExpected = { "a","b","c" };
+        assert(vecExpected == vecResult);
+    }
+}
+
+int main()
+{
+    test();
+    std::cout << "Hello World!\n";
+}
---
+++ LeetCode/41_FirstMissingPositive/41_FirstMissingPositive.cpp
@@ -0,0 +1,80 @@
+﻿// 41_FirstMissingPositive.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include "../global.h"
+
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        if (nums.size() == 0)
+            return 1;
+
+        set<int> setNums;
+        for (auto num : nums)
+        {
+            if (num > 0)
+            {
+                setNums.insert(num);
+            }
+        }
+
+        auto itBegin = setNums.begin();
+        if (*itBegin > 1)
+        {
+            return 1;
+        }
+        
+        int nBeginElement = *itBegin + 1;
+        while (setNums.find(nBeginElement) != setNums.end())
+        {
+            ++nBeginElement;
+        }
+        return nBeginElement;
+    }
+};
+
+void test()
+{
+    {
+        Solution solu;
+        vector<int> nums = {};
+        int nResult = solu.firstMissingPositive(nums);
+        int nExpected = 1;
+        assert(nExpected == nResult);
+    }
+    {
+        Solution solu;
+        vector<int> nums = { 1,2 };
+        int nResult = solu.firstMissingPositive(nums);
+        int nExpected = 3;
+        assert(nExpected == nResult);
+    }
+    {
+        Solution solu;
+        vector<int> nums = { 1,2,0 };
+        int nResult = solu.firstMissingPositive(nums);
+        int nExpected = 3;
+        assert(nExpected == nResult);
+    }
+    {
+        Solution solu;
+        vector<int> nums = { 3,4,-1,1 };
+        int nResult = solu.firstMissingPositive(nums);
+        int nExpected = 2;
+        assert(nExpected == nResult);
+    }
+    {
+        Solution solu;
+        vector<int> nums = { 7,8,9,11,12};
+        int nResult = solu.firstMissingPositive(nums);
+        int nExpected = 1;
+        assert(nExpected == nResult);
+    }
+}
+
+int main()
+{
+    test();
+    std::cout << "Hello World!\n";
+}
